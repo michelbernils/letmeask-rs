8c9f13ff2820abb9e03790ff21386139f2250474
Michel <bernils.michel@gmail.com>
2021-07-25 23:28:22 -0300

    Aula 3 concluida

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { BrowserRouter, Route } from 'react-router-dom';
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import {Home} from './pages/Home';
 import {NewRoom} from './pages/NewRoom';
+import {Room} from './pages/Room';
 
 
 
@@ -15,8 +15,11 @@ function App() {
   return (
       <BrowserRouter>
           <AuthContextsProvider>
-            <Route path="/" exact component={Home} />
-            <Route path="/rooms/new" component={NewRoom} />
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/rooms/new" component={NewRoom} />
+              <Route path="/rooms/:id" component={Room} />
+            </Switch>
           </AuthContextsProvider>
       </BrowserRouter>
   );
